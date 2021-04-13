9ec3955991abd4ed5c4540d4822afb21520a3ac3
Hanam Lee <stxz00naver.com>
2021-04-13 18:22:35 +0900

    4/13 대폭 수정 예정 account 시큐리티 문제
    Vo 등 갈아엎을 예정이지만 아직 사용하는 데 있어서는 앞선 행동이 되므로 수정은 미루되, 조인할 생각을 해보도록 함
    상위 account, 하위 memberaccount 외 2개, 각 accountinfo들을 매퍼 통합해서 사용할지도 여부 굉장히 생각해볼 필요도 있음.
    
    조인 부분 때문에 매우 신중하게 매퍼 설정해야함.

--- finalproject/src/main/java/com/bit/fn/config/WebSecurityConfig.java
+++ finalproject/src/main/java/com/bit/fn/config/WebSecurityConfig.java
@@ -1,32 +1,79 @@
 package com.bit.fn.config;
 
+import javax.sql.DataSource;
+
+import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
+			//로그인 인증 없어도 접근 가능한 영역
 				.antMatchers("/", "/home","/index","/join").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
+			//로그인 없이 접근 시 이동하는 경로
 				.loginPage("/login")
 				.permitAll()
 				.and()
 			.logout()
+			//로그아웃은 누구나 가능하다
 				.permitAll();
 	}
 
+@Autowired
+public void configureGlobal(AuthenticationManagerBuilder auth) 
+  throws Exception {
+    auth.jdbcAuthentication()
+      .dataSource(dataSource)
+      //비밀번호 암호화 반영
+      .passwordEncoder(passwordEncoder())
+      //인증처리
+      .usersByUsernameQuery("SELECT id,PASSWORD,enabled "
+      		+ "from adminaccount "
+      		+ "WHERE id = ? "
+      		+ "UNION "
+      		+ "SELECT id,PASSWORD,enabled "
+      		+ "from "
+      		+ "memberaccount "
+      		+ "WHERE id = ? "
+      		+ "UNION "
+      		+ "SELECT "
+      		+ "id,PASSWORD,enabled "
+      		+ "from "
+      		+ "masteraccount "
+      		+ "WHERE id = ? ")
+      //권한처리
+      .authoritiesByUsernameQuery("select id,authority "
+        + "from authorities "
+        + "where id = ?");
+}	
+
+//Authentication = 로그인
+//Authorization = 권한
+
 	
+@Bean
+public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+}
+
+
 }
