e467fb14cf0bb825c2307fdc0e77002093cdd16e
yonghun2 <77871575+yonghun2@users.noreply.github.com>
2021-04-14 15:55:57 +0900

    detail, bbs

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -1,11 +1,29 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
 
-	public String index() {
-		return "";
+	@RequestMapping("/index")
+	public String main() {
+		return "index";
 	}
+	
+	@RequestMapping("/bbs")
+	public String bbs() {
+		return "bbs";
+	} 
+	
+	@RequestMapping("/detail")
+	public String detail() {
+		return "detail";
+	}
+	
+	@RequestMapping("/sample")
+	public String sample() {
+		return "sample";
+	}
+	
 }
