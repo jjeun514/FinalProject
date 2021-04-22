7dd2086c36d53b23b0132faf8f70d55492bbb35c
luna <dichotomy.bgm@gmail.com>
2021-04-22 11:15:17 +0900

    I don't know how to use ajax ..... lol

--- finalproject/src/main/java/com/bit/fn/controller/MemberController.java
+++ finalproject/src/main/java/com/bit/fn/controller/MemberController.java
@@ -1,6 +1,9 @@
 package com.bit.fn.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -58,12 +61,23 @@ public class MemberController {
 	// 멤버 파트 회의실 예약 신청에 필요한 정보 전달
 	@RequestMapping(value = "/reservation/apply", method = RequestMethod.GET)
 	@ResponseBody
-	public List<ReservationVo> roomInfo(Model model) {
+	public Map<String, Object> roomInfo() {
 		
+		List<Map<String, String>> dataList = new ArrayList<Map<String,String>>();
+		
+		Map<String, String> data = null;
 		List<ReservationVo> roomList = service.mettingRoomList();
-		model.addAttribute("roomList", roomList);
 		
-		return roomList;
+		for(ReservationVo REZ : roomList) {
+			data = new HashMap<String, String>();
+			data.put("roomNum", Integer.toString(REZ.getRoomNum()));
+			dataList.add(data);
+		}
+		
+		Map<String, Object> roomData = new HashMap<String, Object>();
+		roomData.put("roomData", dataList);
+		
+		return roomData;
 	}
 	
 	// 멤버 파트 내 스케쥴 관리 인트로 페이지
