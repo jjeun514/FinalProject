3dc6046fe1e6d183ca348626a649ed445e47dc3f
stxz00 <stxz00@naver.com>
2021-05-13 11:19:12 +0900

    업로드 오류 수정

--- finalproject/src/main/java/com/bit/fn/security/service/AccountService.java
+++ finalproject/src/main/java/com/bit/fn/security/service/AccountService.java
@@ -50,18 +50,16 @@ public class AccountService {
 			e.printStackTrace();
 		}
 		
-		//멤버 회원가입
-	public Account memverSave(Account account) {
 		//패스워드 단방향 암호화 설정
-		String encodedPassword = passwordEncoder.encode(account.getPassword());
+		String encodedPassword = passwordEncoder.encode(tempPassword);
 		account.setPassword(encodedPassword);
 		
-		//enabled 사용가능 컬럼 0 반영
-		account.setEnabled(false);
+		//enabled 사용가능 컬럼 1 반영i
+		account.setEnabled(true);
 		
 		//권한 구분 1(어드민) 설정
 		Role role = new Role();
-		role.setNum(3);
+		role.setNum(2);
 		account.getRoles().add(role);
 		
 		//설정한 값으로 MariaDB 값 저장(회원가입)
