844ce5b9c5def271ee16988104fe9dc314b8e10a
stxz00 <stxz00@naver.com>
2021-05-20 09:02:19 +0900

    회의실 관리 구현

--- finalproject/src/main/java/com/bit/fn/controller/MeetingRoomMgmtController.java
+++ finalproject/src/main/java/com/bit/fn/controller/MeetingRoomMgmtController.java
@@ -1,16 +1,23 @@
 package com.bit.fn.controller;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.bit.fn.model.service.ReservationService;
+import com.bit.fn.model.service.BranchService;
+import com.bit.fn.model.service.join.MeetingRoomAndBranchService;
 import com.bit.fn.model.service.join.ReservationListService;
-import com.bit.fn.model.vo.ReservationVo;
+import com.bit.fn.model.vo.MeetingRoomMgmtVo;
 import com.bit.fn.model.vo.join.ReservationListVo;
 
 @Controller
@@ -18,6 +25,12 @@ import com.bit.fn.model.vo.join.ReservationListVo;
 public class MeetingRoomMgmtController {
 	@Autowired
 	ReservationListService reservationListService;
+	
+	@Autowired
+	MeetingRoomAndBranchService meetingRoomAndBranchService;
+	
+	@Autowired
+	BranchService branchService;
 	
 	@RequestMapping("meetingRoomMgmt")
 	public String meetingRoomMgmt(Model model) {
@@ -29,6 +42,109 @@ public class MeetingRoomMgmtController {
 		}
 		
 		model.addAttribute("revList",revList);
+		model.addAttribute("mrList",meetingRoomAndBranchService.selectAll());
+		model.addAttribute("branchList",branchService.selectAll());
 		return "meetingRoomMgmt";
+	}
+	
+	@RequestMapping(value = "selectRevOne", method = RequestMethod.POST)
+	@ResponseBody
+	public HashMap<String, Object> selectRevOne(MeetingRoomMgmtVo selectRev) {
+		//System.out.println(selectRev);
+		
+		String memNickName = selectRev.getMemNickName();
+		String reservationDay = selectRev.getReservationDay();
+		String useStartTime = selectRev.getUseStartTime();
+		
+		ReservationListVo revOne = reservationListService.selectOneJoin(memNickName, reservationDay, useStartTime);
+		
+		//select할 reservationList를 생성
+		HashMap<String, Object> result = new HashMap<String, Object>();
+		result.put("memName", revOne.getMemberInfo().getMemName());
+		result.put("id", revOne.getMemberInfo().getId());
+		result.put("roomNum", revOne.getReservation().getRoomNum());
+		result.put("reservationDay", revOne.getReservation().getReservationDay());
+		result.put("useStartTime", revOne.getReservation().getUseStartTime().substring(11,16));
+		result.put("useFinishTime", revOne.getReservation().getUseFinishTime().substring(11,16));
+		result.put("userCount", revOne.getReservation().getUserCount());
+		result.put("fee", revOne.getReservation().getAmount());
+		result.put("comCode", revOne.getCompanyInfo().getComCode());
+		result.put("comName", revOne.getCompanyInfo().getComName());
+		result.put("ceo", revOne.getCompanyInfo().getCeo());
+		result.put("manager", revOne.getCompanyInfo().getManager());
+		result.put("comPhone", revOne.getCompanyInfo().getComPhone());
+		result.put("dept", revOne.getMemberInfo().getDept());
+		result.put("memPhone", revOne.getMemberInfo().getMemPhone());
+		result.put("merchant_uid", revOne.getReservation().getMerchant_uid());
+		
+		
+		return result;
+	}
+	
+	@PutMapping("updateReservation")
+	@ResponseBody
+	public String updateReservation( 
+									int updateRoomNum, String updateReservationDay, String updateUseStartTimValue,
+									String updateUseFinishTimeValue, int updateUserCountValue, int updateFeeValue,
+									String reservationDay, String memNickName, String useStartTime) {
+		int result = reservationListService.updateReservation(updateRoomNum, updateReservationDay, updateUseStartTimValue, updateUseFinishTimeValue,
+				updateUserCountValue, updateFeeValue, memNickName, reservationDay, useStartTime);
+		System.out.println(result);
+		if(result==1) {
+			return "updated";
+		}
+		
+		return "false";
+	}
+
+	@DeleteMapping("deleteReservation")
+	@ResponseBody
+	public String deleteReservation(int roomNum, String reservationDay, String useStartTime) {
+		int result = reservationListService.deleteReservation(roomNum,reservationDay, useStartTime);
+		System.out.println(result);
+		if(result==1) {
+			return "deleted";
+		}
+		
+		return "false";
+	}
+	
+	@PutMapping("updateMeetingRoom")
+	@ResponseBody
+	public String updateMeetingRoom(String acreagesValue, int rentValue, int maxValue, int roomNum,  String branchName) {
+		System.out.println(acreagesValue);
+		System.out.println(rentValue);
+		System.out.println(maxValue);
+		System.out.println(roomNum);
+		System.out.println(branchName);
+		int result = meetingRoomAndBranchService.updateMeetingRoom(acreagesValue, rentValue, maxValue, roomNum, branchName);
+		if(result==1) {
+			return "updated";
+		}
+		return "false";
+	}
+	
+	@DeleteMapping("deleteMeetingRoom")
+	@ResponseBody
+	public String deleteMeetingRoom(int roomNum, String branchName) {
+		System.out.println(branchService.selectBranchCode(branchName).get(0).get("branchCode"));
+		int branchCode=(int) branchService.selectBranchCode(branchName).get(0).get("branchCode");
+		int result = meetingRoomAndBranchService.deleteMeetingRoom(roomNum, branchCode);
+		System.out.println(result);
+		if(result==1) {
+			return "deleted";
+		}
+		
+		return "false";
+	}
+	
+	@PostMapping("addMeetingRoom")
+	@ResponseBody
+	public String addMeetingRoom(int branchNameValue, int roomNumValue, String acreagesValue, int rentValue, int maxValue) {
+		int result = meetingRoomAndBranchService.addMeetingRoom(roomNumValue, branchNameValue, acreagesValue, rentValue, maxValue);
+		if(result==1) {
+			return "added";
+		}
+		return "false";
 	}
 }
--- finalproject/src/main/webapp/WEB-INF/views/myPage.jsp
+++ finalproject/src/main/webapp/WEB-INF/views/myPage.jsp
@@ -324,7 +324,7 @@
 										$('#dangerModal').modal('show');
 									}else if(nickNameLengthBoo==true && nickNameCharacterBoo==true){
 										$.ajax({
-											url: "/nickNameCheck",
+											url: "/nickNameChecsk",
 											type : "POST",
 											data: {memNickName},
 											contentType : "application/x-www-form-urlencoded; charset=UTF-8",
@@ -444,6 +444,9 @@
 				document.getElementById('modalText01').textContent='숫자,영어,특수문자를 포함하여 비밀번호를 입력해주세요.';
 				$('#dangerModal').modal('show');
 				}
+			}else if($('.newPw').val()==$(".existingPw").val()){
+				document.getElementById('modalText01').textContent='기존 비밀번호와 다른 비밀번호를 입력해주세요.';
+				$('#dangerModal').modal('show');
 			}else{
 				//$(".usablePw").remove();
 				//$(".unusablePw").remove();
